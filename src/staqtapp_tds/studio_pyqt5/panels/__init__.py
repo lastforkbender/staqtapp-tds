3c07e2466a1923840c126a4a219fae2eaf208223
ROBERT CECIL <146335780+lastforkbender@users.noreply.github.com>
2026-07-07 01:37:29 -0500

    Add panel descriptors to __init__.py

---
+++ src/staqtapp_tds/studio_pyqt5/panels/__init__.py
@@ -0,0 +1,4 @@
+"""Panel descriptors for the v3.1.12 Driver Studio cockpit shell."""
+from .descriptors import STUDIO_PANEL_DESCRIPTORS, StudioPanelDescriptor, panel_descriptor
+
+__all__ = ["STUDIO_PANEL_DESCRIPTORS", "StudioPanelDescriptor", "panel_descriptor"]
