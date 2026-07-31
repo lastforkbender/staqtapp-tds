13e21efafe3674c7c3f92dcfed091c3d14f453d6
ROBERT CECIL <146335780+lastforkbender@users.noreply.github.com>
2026-07-31 04:53:32 -0500

    Expose checksum and UTF-8 truth contracts

--- src/staqtapp_tds/native/__init__.py
+++ src/staqtapp_tds/native/__init__.py
@@ -9,6 +9,23 @@ from staqtapp_tds.native.manager import (
     native_capabilities_result,
 )
 
+from staqtapp_tds.native.checksums import (
+    CHECKSUM32_ALGORITHMS,
+    CRC32_IEEE_V1,
+    DEFAULT_CHECKSUM32_ALGORITHM,
+    FNV1A32_LEGACY_V1,
+    ChecksumAlgorithmError,
+    checksum32,
+    checksum32_many,
+    checksum32_python,
+    manifest_checksum32_algorithm,
+)
+from staqtapp_tds.native.utf8 import (
+    UTF8_CHUNK_CONTRACT,
+    utf8_chunk_bounds,
+    utf8_chunk_bounds_python,
+)
+
 __all__ = [
     "NativeEngineManager",
     "NativeLoadReport",
@@ -17,4 +34,16 @@ __all__ = [
     "get_native_manager",
     "native_status_result",
     "native_capabilities_result",
+    "CHECKSUM32_ALGORITHMS",
+    "CRC32_IEEE_V1",
+    "DEFAULT_CHECKSUM32_ALGORITHM",
+    "FNV1A32_LEGACY_V1",
+    "ChecksumAlgorithmError",
+    "checksum32",
+    "checksum32_many",
+    "checksum32_python",
+    "manifest_checksum32_algorithm",
+    "UTF8_CHUNK_CONTRACT",
+    "utf8_chunk_bounds",
+    "utf8_chunk_bounds_python",
 ]
